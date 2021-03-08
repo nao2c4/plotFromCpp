3ef6dfb6e4910eba0e9f43109fdc11199aa110bf
nao2C4 <27849770+nao2c4@users.noreply.github.com>
2021-03-08 17:14:25 +0900

    [update] コメント追加

--- plot.py
+++ plot.py
@@ -4,13 +4,23 @@ import matplotlib.pyplot as plt
 
 
 if __name__ == '__main__':
+    # Figure作成
     fig = plt.figure()
+    # 軸作成
     ax = fig.add_subplot()
+    # 入力待ち (1回目)
     path = input()
+    # ループ
+    # 普通ファイル名は4文字以上が多いため適当に条件を設定
     while len(path) > 3:
+        # ファイル読み込み
         data = np.loadtxt(path)
+        # プロット
         ax.plot(data)
-        # fig.show()
+        # 表示
+        # こうすると途中でグラフを表示可能らしい
         plt.pause(0.001)
+        # 入力待ち
         path = input()
+        # 次のプロットのために表示消す
         ax.clear()
